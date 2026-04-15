f1bec83e054f6ae47fad32d9e089213084a37de6
everterrachanghua-maker <everterra.changhua@gmail.com>
2026-04-15 16:24:07 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // 構建時忽略 ESLint 檢查
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // 構建時忽略 TypeScript 類型錯誤
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
-
 export default nextConfig;
